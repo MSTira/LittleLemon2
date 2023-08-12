6a83213792981b80a19634264624389ee95a6f08
MSusana <mstiraboschi@gmail.com>
2023-08-12 16:14:20 -0300

    API para Menu y Booking

--- restaurant/models.py
+++ restaurant/models.py
@@ -11,12 +11,3 @@ class Booking(models.Model):
     numberOfGuest=models.IntegerField()
     bookingDate=models.DateTimeField()
     
-class Menu2(models.Model):
-    title=models.CharField(max_length=255)
-    price=models.DecimalField(max_digits=10, decimal_places=2)
-    inventory=models.IntegerField()    
-    
-class Booking2(models.Model):
-    name=models.CharField(max_length=255)
-    numberOfGuest=models.IntegerField()
-    bookingDate=models.DateTimeField()
